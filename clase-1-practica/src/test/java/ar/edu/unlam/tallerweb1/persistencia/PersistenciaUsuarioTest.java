952027adc1a68c99bb192b7306c9ed8098fcb801
Dani <marconidaniel@outlook.com>
2018-04-07 16:21:04 -0300

    Tarea clase 1: testing

--- clase-1-practica/src/test/java/ar/edu/unlam/tallerweb1/persistencia/PersistenciaUsuarioTest.java
+++ clase-1-practica/src/test/java/ar/edu/unlam/tallerweb1/persistencia/PersistenciaUsuarioTest.java
@@ -3,24 +3,64 @@ package ar.edu.unlam.tallerweb1.persistencia;
 import ar.edu.unlam.tallerweb1.SpringTest;
 import ar.edu.unlam.tallerweb1.modelo.Usuario;
 import org.hibernate.Session;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.*;
 
 public class PersistenciaUsuarioTest extends SpringTest {
+    private Usuario usuario;
+    private Session session;
 
-    // EJEMPLO DE TEST DE PERSISTENCIA
+    @Before
+    public void setUp(){
+        usuario = new Usuario();
+        usuario.setEmail("email@empresa.com");
+        usuario.setPassword("password");
+        usuario.setRol("Usuario");
+
+        session = getSession();
+    }
+
     @Test
     @Transactional @Rollback()
     public void alGuardarUnUsuarioDeberiaEstarEnLaBase(){
-        Usuario usuarioPrueba = new Usuario();
-        usuarioPrueba.setEmail("john@doe.com");
-        usuarioPrueba.setPassword("lele");
-        usuarioPrueba.setRol("LÍDER");
-        Session session = getSession();
-        session.save(usuarioPrueba); // Guarda el ID
-        Usuario usuarioBuscado = session.get(Usuario.class, usuarioPrueba.getId());
+        session.save(usuario); // Guarda el ID
+        Usuario usuarioBuscado = session.get(Usuario.class, usuario.getId());
+
         assertThat (usuarioBuscado).isNotNull();
+        assertThat(usuarioBuscado.getId()).isEqualTo(usuario.getId());
+        assertThat(usuarioBuscado.getEmail()).isEqualTo(usuario.getEmail());
+        assertThat(usuarioBuscado.getPassword()).isEqualTo(usuario.getPassword());
+        assertThat(usuarioBuscado.getRol()).isEqualTo(usuario.getRol());
+    }
+
+    @Test
+    @Transactional @Rollback()
+    public void alActualizarUnUsuarioDeberiaEstarActualizadoEnLaBase(){
+        session.save(usuario);
+
+        usuario.setEmail("nuevo_email@empresa.com");
+        usuario.setPassword("nueva_password");
+        usuario.setRol("Administrador");
+
+        session.save(usuario);
+
+        Usuario usuarioBuscado = session.get(Usuario.class, usuario.getId());
+
+        assertThat(usuarioBuscado.getId()).isEqualTo(usuario.getId());
+        assertThat(usuarioBuscado.getEmail()).isEqualTo(usuario.getEmail());
+        assertThat(usuarioBuscado.getPassword()).isEqualTo(usuario.getPassword());
+        assertThat(usuarioBuscado.getRol()).isEqualTo(usuario.getRol());
+    }
+
+    @Test
+    @Transactional @Rollback()
+    public void alEliminarUnUsuarioNoDeberiaEstarEnLaBase(){
+        session.save(usuario);
+        session.delete(usuario);
+        Usuario usuarioBuscado = session.get(Usuario.class, usuario.getId());
+        assertThat(usuarioBuscado).isNull();
     }
 }
